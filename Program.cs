888eee52de1234b012f9e6fb64fe53ccf305f5e7
Fernando Mazariegos <nandomf46@gmail.com>
2025-08-03 00:20:41 -0600

    Ejercicios vistos en clase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using EjerciciosEnClase1.clases;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Empleado emp = new Empleado();
+
+        emp.Nombre = "Luis";
+        emp.Salario = 5000;
+
+        Console.WriteLine($"Nombre: {emp.Nombre}");
+        Console.WriteLine($"Salario: {emp.Salario}");
+
+
+        emp.Nombre = "";       
+        emp.Salario = -1000;
+
+        Console.ReadKey();
+    }
+}
---
+++ clases/Empleado.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosEnClase1.clases
+{
+    public class Empleado
+    {
+        private string _nombre = "";
+        private decimal _salario;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    _nombre = value;
+                else
+                    Console.WriteLine("El nombre no puede estar en blanco.");
+            }
+        }
+
+        public decimal Salario
+        {
+            get { return _salario; }
+            set
+            {
+                if (value >= 0)
+                    _salario = value;
+                else
+                    Console.WriteLine("El salario no puede ser negativo.");
+            }
+        }
+    }
+}
